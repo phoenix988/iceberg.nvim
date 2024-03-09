86a60ab14145a4c155b33d5dc42dc46a460db016
phoenix988 <karlfredin@gmail.com>
2024-03-09 21:33:12 +0200

    testing colors

--- lua/iceberg/palette.lua
+++ lua/iceberg/palette.lua
@@ -23,7 +23,7 @@ return {
     dark_red = '#E27878',
     dark_yellow = '#C7895C',
     dark_purple = '#7A6B9D',
-    diff_add = '#C0CA8E',
+    diff_add = '#7A6B9D',
     diff_delete = '#C25C5C',
     diff_change = '#5E7B9D',
     diff_text = '#2D3D50',
